a8f5210ab85001236e2a32741e5a04204339fe5b
Orazbek <114831982+orazdaurenov@users.noreply.github.com>
2024-11-20 17:21:08 +0000

    added grid

--- src/app/components/Grid.tsx
+++ src/app/components/Grid.tsx
@@ -3,23 +3,31 @@ import React from "react";
 const Grid = () => {
   return (
     <>
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <div className="grid-cols-1">
+        <Square value={"X"} />
+        <Square value={"X"} />
+        <Square value={"X"} />
+      </div>
+      <div className="grid-cols-1">
+        <Square value={"X"} />
+        <Square value={"X"} />
+        <Square value={"X"} />
+      </div>
+      <div className="grid-cols-1">
+        <Square value={"X"} />
+        <Square value={"X"} />
+        <Square value={"X"} />
+      </div>
     </>
   );
 };
 
-const Square = () => {
+const Square = ({ value }) => {
   return (
     <>
-      <div className="h-10 w-10 border-2 border-solid border-black"></div>
+      <div className="h-10 w-10 border-2 border-solid border-black">
+        {value}
+      </div>
     </>
   );
 };
