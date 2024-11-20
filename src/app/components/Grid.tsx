d6c5d393350d61f73afbba7d2868a809a97edb6c
Orazbek <114831982+orazdaurenov@users.noreply.github.com>
2024-11-20 17:30:17 +0000

    minor change

--- src/app/components/Grid.tsx
+++ src/app/components/Grid.tsx
@@ -2,27 +2,27 @@ import React from "react";
 
 const Grid = () => {
   return (
-    <>
-      <div className="grid-cols-1">
+    <div className="flex justify-center">
+      <div className="grid">
         <Square value={"X"} />
         <Square value={"X"} />
         <Square value={"X"} />
       </div>
-      <div className="grid-cols-1">
+      <div className="grid">
         <Square value={"X"} />
         <Square value={"X"} />
         <Square value={"X"} />
       </div>
-      <div className="grid-cols-1">
+      <div className="grid">
         <Square value={"X"} />
         <Square value={"X"} />
         <Square value={"X"} />
       </div>
-    </>
+    </div>
   );
 };
 
-const Square = ({ value }) => {
+const Square = ({ value }: { value: string }) => {
   return (
     <>
       <div className="h-10 w-10 border-2 border-solid border-black">
